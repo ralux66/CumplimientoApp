81f7946d0be0938fde8af069c76e97a88e41462d
rzepeda <rzepeda@STSV-WRKS89.statetrust.sv>
2019-05-24 10:47:04 -0600

    Commit de loggin para notarios y abogados.

--- ComplianceWeb/src/main/java/com/avanceti/compliance/controller/NotarioAutoController.java
+++ ComplianceWeb/src/main/java/com/avanceti/compliance/controller/NotarioAutoController.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.avanceti.compliance.model.ActiveMenu;
 import com.avanceti.compliance.model.Notaut;
+import com.avanceti.compliance.model.User;
 import com.avanceti.compliance.services.INotariosService;
 import com.avanceti.compliance.utility.JaroWinklerDistance;
+import com.avanceti.compliance.utility.ValidateUrlRequest;
 
 @Controller
 @RequestMapping("/notario-auto")
@@ -26,15 +31,21 @@ public class NotarioAutoController {
 	private INotariosService notarioService;
 	
 	@GetMapping(value = "/search")
-	public String homeSearch() {
-		menuActive.setSearch("k-menu__item--open k-menu__item--here");			
+	public String homeSearch(Model model,@SessionAttribute("user") User user, HttpServletRequest request) {
+		menuActive.setSearch("k-menu__item--open k-menu__item--here");
+		if (!ValidateUrlRequest.validateUrlMenus(user, request.getServletPath())) {					
+			return "redirect:/error/errorpage";
+		}
 		return "notario/search-auto";
 	}
 	
 	@GetMapping(value = "/listanotario-auto")
-	public String listBlacklist(Model model) {
+	public String listBlacklist(Model model,@SessionAttribute("user") User user, HttpServletRequest request) {
 		model.addAttribute("allnotario", notarioService.allNotarios());	
 		menuActive.setSearch("k-menu__item--open k-menu__item--here");
+		if (!ValidateUrlRequest.validateUrlMenus(user, request.getServletPath())) {					
+			return "redirect:/error/errorpage";
+		}
 		return "notario/listnotario-auto";
 	}
 	
